a89943bf594ef09d502b58a8f6bea1aaea82d406
yvesprogrammeur <yvesprogrammeur@gmail.com>
2025-01-14 09:07:08 +0100

    ajout des classe et props dans les component

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,29 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 // import './App.css'
 
-function App() {
+export default function App() {
   return (
     <>
-      <p>good piko</p>
+    <div className='p-4 flex flex-col gap-4'>
+      <Lago color="blue"/>
+      <Lago color="red"/>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+function Lago(props) {
+  let classes = "h-16 w-32"; 
+
+  if (props.color === "blue") {
+    classes += " bg-blue-500";
+  }
+
+  if (props.color === "red") {
+    classes += " bg-red-500";
+  }
+
+  return <div className={classes}></div>;
+}
+
+
